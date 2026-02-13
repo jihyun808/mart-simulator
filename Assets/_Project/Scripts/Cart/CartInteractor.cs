7ba59ccdf208593c789662ae80c861b025e5ceeb
kimminhee563 <drdk0204@naver.com>
2026-02-14 02:13:09 +0900

    Feature/minhee panel (#34)
    * Revert Stage-1 scene to commit 255ad75
    
    * feat; 탑패널 수정 완료

--- Assets/_Project/Scripts/Cart/CartInteractor.cs
+++ Assets/_Project/Scripts/Cart/CartInteractor.cs
@@ -87,11 +87,6 @@ public class CartInteractor : MonoBehaviour
         }
     }
 
-    public bool IsCartMounted()
-{
-    return mounted != null;
-}
-
     bool FindNearestCart(out CartMount nearest)
     {
         nearest = null;
